9840a7bb5e2e725d77f69fa7119b23f4d38ff84c
Mihails Akimenko <mihails.akimenko@gmail.com>
2017-11-15 17:57:25 +0200

    intergrating server with client

--- mem-server/src/main/java/com/makimenko/mem/server/api/ProfileApiController.java
+++ mem-server/src/main/java/com/makimenko/mem/server/api/ProfileApiController.java
@@ -1,10 +1,10 @@
 package com.makimenko.mem.server.api;
 
 import com.makimenko.mem.server.model.Profile;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
-
-import org.joda.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,39 +14,69 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.threeten.bp.LocalDate;
 
 import javax.validation.constraints.*;
 import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-15T15:41:35.284+02:00")
 
 @Controller
 public class ProfileApiController implements ProfileApi {
 
+	private static final Logger log = LoggerFactory.getLogger(ProfileApiController.class);
+
+	private final ObjectMapper objectMapper;
+
+	private final HttpServletRequest request;
+
+	@org.springframework.beans.factory.annotation.Autowired
+	public ProfileApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+		this.objectMapper = objectMapper;
+		this.request = request;
+	}
+
 	public ResponseEntity<List<Profile>> getAllProfiles() {
+		String accept = request.getHeader("Accept");
+		if (accept != null && accept.contains("application/json")) {
+			try {
+				return new ResponseEntity<List<Profile>>(objectMapper.readValue(
+						"[ {  \"name\" : \"name\",  \"active\" : true,  \"birthDate\" : \"2000-01-23\",  \"userCode\" : \"userCode\"}, {  \"name\" : \"name\",  \"active\" : true,  \"birthDate\" : \"2000-01-23\",  \"userCode\" : \"userCode\"} ]",
+						List.class), HttpStatus.NOT_IMPLEMENTED);
+			} catch (IOException e) {
+				log.error("Couldn't serialize response for content type application/json", e);
+				return new ResponseEntity<List<Profile>>(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+
 		List<Profile> list = new ArrayList<>();
 		Profile profile1 = new Profile();
 		profile1.setName("Michael");
 		profile1.setActive(true);
-		profile1.setBirthDate(new LocalDate());
+		profile1.setBirthDate(LocalDate.now());
 		profile1.setUserCode("user1");
 		list.add(profile1);
 
 		Profile profile2 = new Profile();
 		profile2.setName("Alex");
 		profile2.setActive(false);
-		profile2.setBirthDate(new LocalDate());
+		profile2.setBirthDate(LocalDate.now());
 		profile2.setUserCode("user2");
 		list.add(profile2);
 
 		ResponseEntity<List<Profile>> response = new ResponseEntity<>(list, HttpStatus.OK);
 		return response;
+
 	}
 
 	public ResponseEntity<Void> profilePost(
 			@ApiParam(value = "Create new profile", required = true) @Valid @RequestBody Profile profile) {
-		// do some magic!
-		return new ResponseEntity<Void>(HttpStatus.OK);
+		String accept = request.getHeader("Accept");
+		return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
 	}
+
 }
